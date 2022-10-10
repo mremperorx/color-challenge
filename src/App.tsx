7b8366a12ed5a72282e351f7adb1d26b1b8069a2
Majed El-Naser <83935574+mremperorx@users.noreply.github.com>
2022-10-10 19:24:50 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [answer, setAnswer] = useState<string[]>([]);
+  const [result, setResult] = useState<boolean | undefined>(undefined);
+
+  const getRandomColor = () => {
+    const digits = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+
+    const color = new Array(6)
+      .fill("")
+      .map(() => digits[Math.floor(Math.random() * digits.length)])
+      .join("");
+
+    return `#${color}`;
+  };
+
+  useEffect(() => {
+    const actualColor = getRandomColor();
+    setColor(actualColor);
+    setAnswer(
+      [actualColor, getRandomColor(), getRandomColor()].sort(
+        () => 0.5 - Math.random()
+      )
+    );
+  }, []);
+
+  const handleAnwserClicked = (answer: string) => {
+    if (answer === color) {
+      setResult(true);
+      generateColor();
+    } else {
+      setResult(false);
+    }
+  };
+
+  const generateColor = () => {
+    const actualColor = getRandomColor();
+    getRandomColor();
+    setColor(actualColor);
+    setAnswer(
+      [actualColor, getRandomColor(), getRandomColor()].sort(
+        () => 0.5 - Math.random()
+      )
+    );
+  };
+
+  useEffect(() => {
+    generateColor();
+  }, []);
+
+  return (
+    <div className="App">
+      <div>
+        <div className="guess" style={{ background: color }}></div>
+
+        {answer.map((answer) => (
+          <button onClick={() => handleAnwserClicked(answer)} key={answer}>
+            {answer}
+          </button>
+        ))}
+
+        {result === false && <div className="worng">Worng! </div>}
+        {result === true && <div className="correct"> Correct! </div>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
